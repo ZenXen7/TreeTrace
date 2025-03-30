0ae14e1e706259fe623fabaa0ded1361ae242fc7
ZenXen7 <karlchristian.ajero@cit.edu>
2025-03-30 21:08:00 +0800

    Worked on Login Page

--- client/app/auth/login/layout.tsx
+++ client/app/auth/login/layout.tsx
@@ -1,31 +1,24 @@
 import type { Metadata } from "next";
 import "../../globals.css";
 
-import { Outfit } from "next/font/google"
+import { Outfit } from "next/font/google";
 
 const outfit = Outfit({
   subsets: ["latin"],
   display: "swap",
   variable: "--font-outfit",
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-})
+});
 
 export const metadata: Metadata = {
-    title: "Login",
-    description: "Login to your account",
-}
+  title: "Login",
+  description: "Login to your account",
+};
 
 export default function LoginLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" className="dark" suppressHydrationWarning>
-      <body className={`min-h-screen bg-black antialiased ${outfit.variable} ${outfit.className}`}>
-        {children}
-      </body>
-    </html>
-  )
+  return <div className={`min-h-screen bg-black antialiased ${outfit.variable} ${outfit.className}`}>{children}</div>;
 }
-
