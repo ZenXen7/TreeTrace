1211029a52fb599a50b951f8684ecdf5632ffc3b
KC <ajajs@yahoolcom>
2025-10-18 22:36:54 +0800

    Fixed build errors

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,5 +1,5 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
@@ -9,7 +9,7 @@ import * as express from 'express';
 import serverlessExpress from '@vendia/serverless-express';
 
 const expressApp = express();
-let server: any;
+let cachedServer: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(
@@ -22,21 +22,17 @@ async function bootstrap() {
     credentials: true,
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
   });
 
   app.useGlobalPipes(new ValidationPipe());
   await app.init();
 
-  // Wrap the express app in a serverless handler
   return serverlessExpress({ app: expressApp });
 }
 
-// Export the serverless handler for Vercel
-export const handler = async (event, context) => {
-  if (!server) {
-    server = await bootstrap();
+export default async function handler(req, res) {
+  if (!cachedServer) {
+    cachedServer = await bootstrap();
   }
-  return server(event, context);
-};
+  return cachedServer(req, res);
+}
