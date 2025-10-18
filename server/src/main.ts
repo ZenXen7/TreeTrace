7cc089064f001e8093600187c0ad2f4308b1a0bf
KC <ajajs@yahoolcom>
2025-10-18 22:28:05 +0800

    [KC] Trying to fix deployment errors

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe, Logger } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  // const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
@@ -11,17 +10,16 @@ async function bootstrap() {
     credentials: true,
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204, // <-- important for OPTIONS
   });
 
   app.useGlobalPipes(new ValidationPipe());
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
-
-  // logger.log(`🚀 Application is running on: http://localhost:${port}`);
 }
 
 bootstrap().catch((err) => {
-  // const logger = new Logger('Bootstrap');
-  // logger.error('❌ Error during bootstrap:', err);
+  console.error('❌ Bootstrap error:', err);
 });
